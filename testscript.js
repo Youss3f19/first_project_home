8c487c80e49d28587111901d879af714fa996403
Youss3f19 <145879036+Youss3f19@users.noreply.github.com>
2023-09-24 22:49:12 +0100

    home

--- testscript.js
+++ testscript.js
@@ -1,15 +1,11 @@
- $(window).scroll(function(){
-    if ($(window).scrollTop()){
-        $("nav").addClass("black");
-        $(".navbar").addClass("navtit");
-    }
-    else{
-        $("nav").removeClass("black");
-        $(".navbar").removeClass("navtit");
-        
-  }
- });
+const wrapper = document.querySelector('.wrapper');
+const login = document.querySelector('.logg');
+const signin = document.querySelector('.sigg');
+const formlogin = document.querySelector('.formlogin');
 
+login.addEventListener('click',() => {
+    wrapper.classList.add('.enable');
+});
 
 function addlog(){
     var wrapper = document.getElementById('wrapper');
@@ -31,13 +27,7 @@ function close(){
     var wrapper = document.getElementById('wrapper');
     var login = document.getElementById('formlogin');
     var formsigin = document.getElementById('formsigin');
-    wrapper.style.opacity= "0";
     wrapper.style.scale = "0";
     login.style.scale = "0";
     formsigin.style.scale ="0";
-
-}
-function toggle(){
-    var blur = document.getElementById('ct');
-    blur.classList.toggle('active');
 }
